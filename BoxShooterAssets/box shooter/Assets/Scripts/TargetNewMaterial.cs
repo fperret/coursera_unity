e57f46d01812e03edef879da5a36f76c1c1aeec8
fperret <ptidinono@gmail.com>
2015-12-03 18:41:59 +0100

    week 3

---
+++ BoxShooterAssets/box shooter/Assets/Scripts/TargetNewMaterial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetNewMaterial : MonoBehaviour {
+
+    public Material broken_material;
+
+    private Renderer renderer;
+
+
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    void OnCollisionEnter (Collision newCollision)
+    {
+        if (broken_material != null)
+        {
+            renderer.material = broken_material;
+            broken_material = null;
+        }
+    }
+}
